e12a45f468c9d90f1dadfdc48bddd612be511be9
winniewambui <winniebwambui7@gmail.com>
2023-08-28 17:58:59 +0300

    modified css

--- script.js
+++ script.js
@@ -17,13 +17,14 @@ function displayProducts(){
         // const productsArray = data.fields;
 
         const productsContainer = data.map(product =>{
-            // console.log(product.fields.image[0]);
+            console.log(product.fields.image[0]);
+            const formatPrice = product.fields.price/100
             return`
             <article class="product">
-            <img src=${product.fields.image[0]} alt="image" class="image">
+            <img src=${product.fields.image[0].url} alt="image" class="image">
     <div class="details">
         <h3 class="name">${product.fields.name}</h3>
-    <h1 class="price">${product.fields.price}</h1>
+    <h1 class="price">$ ${formatPrice}</h1>
     </div>
         </article>
             `
