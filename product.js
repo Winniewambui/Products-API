142ade3f96adc6e70d897611530cade338714638
winniewambui <winniebwambui7@gmail.com>
2023-08-29 16:35:19 +0300

    added media queries

--- product.js
+++ product.js
@@ -8,16 +8,20 @@ function displaySingleProduct() {
     const urlParams = new URLSearchParams(window.location.search);
     const productId = urlParams.get('id');
 
-    fetch(url, {
+    fetch(`${url}?id=${productId}`, { // Fetch the single product by its ID
         headers: {
             'Accept': 'application/json',
         },
     }).then((response) => {
         return response.json();
-    }).then((data) => {
-        const singleProduct = data.find(product => product.id === productId);
+    }).then((singleProduct) => { // Use the fetched singleProduct directly
         if (singleProduct) {
             const formatPrice = singleProduct.fields.price / 100;
+            const colorsList = singleProduct.fields.colors
+            .map((color) => {
+              return `<span class="product-color" style="background: ${color}"></span>`;
+            })
+            .join('');
 
             const productContainer = `
                 <article class="single-product">
@@ -26,13 +30,15 @@ function displaySingleProduct() {
                         <h3 class="product-name">${singleProduct.fields.name}</h3>
                         <h3 class="product-company">${singleProduct.fields.company}</h3>
                         <h1 class="product-price">$ ${formatPrice}</h1>
-                        <h3 class="product-colors">${singleProduct.fields.colors}</h3>
-                        <p class="product-desc">${singleProduct.fields.description}</p>
+                        <h3 class="product-colors">${colorsList}</h3>
+                        <p class="product-desc">${singleProduct.fields.description}</p> 
+                        <button class="cart-btn">add to cart</button>
                     </div>
                 </article>
             `;
 
-            document.querySelector('.single-product').innerHTML = productContainer;
+            document.querySelector('.product-container').innerHTML = productContainer;
         }
     });
 }
+
